91f1e81be88c45cfacb9f13a5e5d7855da6621a1
Jay Wu <>
2021-03-19 16:24:58 +0800

    fix

--- src/components/WeatherApp/components/WeatherIcon.tsx
+++ src/components/WeatherApp/components/WeatherIcon.tsx
@@ -99,7 +99,7 @@ const WeatherIcon = (props: WeatherIconProps) => {
     if (theWeatherIcon !== undefined) {
       setCurrentWeatherIcon(theWeatherIcon);
     }
-  }, [currentWeatherCode]);
+  }, [theWeatherIcon]);
 
   return (
     <IconContainer>
